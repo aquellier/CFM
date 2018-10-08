0e94dbb22b5a7f40d4a3c3806bec2d0dcbd183f5
antoinequellier <antoinecquellier@gmail.com>
2018-10-08 22:12:58 +0100

    add basic user and bookings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_133339) do
+ActiveRecord::Schema.define(version: 2018_10_08_211209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.datetime "date"
@@ -22,6 +27,18 @@ ActiveRecord::Schema.define(version: 2018_10_08_133339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
+    t.string "date_of_birth"
+    t.string "nationality"
+    t.string "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
