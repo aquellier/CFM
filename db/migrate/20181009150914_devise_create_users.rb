2265db80d9618de2d5297164f6aa6406164d5b8e
antoinequellier <antoinecquellier@gmail.com>
2018-10-09 16:17:56 +0100

    migration new user model

--- db/migrate/20181009150914_devise_create_users.rb
+++ db/migrate/20181009150914_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name
+      t.string :last_name
+      t.string :nationality
+      t.string :date_of_birth
+      t.string :position
 
       ## Recoverable
       t.string   :reset_password_token
