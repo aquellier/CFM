ba588166c38dc3e3f00a7f789785c1249938bc3e
antoinequellier <antoinecquellier@gmail.com>
2018-10-10 18:46:27 +0100

    User can add its own photo on signup form

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'redis'
 gem 'devise'
 gem 'figaro'
 gem 'cloudinary'
+gem 'carrierwave', '~> 1.2'
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :position, :photo])
   end
 end
