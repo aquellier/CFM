5fd1a9988da9a3c3f22ef0eea95c0bade30ac1d1
antoinequellier <antoinecquellier@gmail.com>
2018-10-12 14:45:16 +0100

    migration bookings references

--- db/migrate/20181008210834_create_bookings.rb
+++ db/migrate/20181008210834_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-
+      t.references :game, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
-    end
   end
 end
