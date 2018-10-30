e662d4518feef00768225877b352f6dd0cf4a0e4
antoinequellier <antoinecquellier@gmail.com>
2018-10-30 10:27:30 +0000

    migration for coordinates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_103906) do
+ActiveRecord::Schema.define(version: 2018_10_30_102601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2018_10_25_103906) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.string "metro_station"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
