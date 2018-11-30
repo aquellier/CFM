101da7c504fa4ebec36c6c3c0162ca9c0f0182e9
antoinequellier <antoinecquellier@gmail.com>
2018-11-30 21:23:37 +0100

    change routes for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,25 +10,30 @@ class BookingsController < ApplicationController
   end
 
   def create
+    # @game = Game.find(params[:game_id])
+    @booking = Booking.new
     @game = Game.find(params[:game_id])
     @booking.game = @game
     @booking.user = current_user
-    @booking = Booking.create!(amount: @booking.game.price, state: 'pending', user: current_user)
+    @booking = Booking.create!(game_id: @booking.game, amount: @booking.game.price, state: 'pending', user: current_user)
+    raise
     if @booking.save
-      # redirect_to new_booking_payment_path(@booking)
+      redirect_to game_path(@game)
     else
       render "games/show"
     end
+    raise
   end
-
-
-
 
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
   end
   private
+
+  def booking_params
+    params.require(:booking).permit(:amount, :state, :user)
+  end
 
   def set_game
     @game = Game.find(params[:game_id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'contact', to: 'pages#contact'
   get '/profile', to: 'pages#profile'
-  resources :games
-  resources :bookings, only: [:show, :new, :create] do
-    resources :payments, only: [:new, :create]
+  resources :games do
+    resources :bookings, only: [ :show, :new, :create] do
+      resources :payments, only: [:new, :create]
+    end
   end
   get '/my_games', to: 'games#my_games'
 end
