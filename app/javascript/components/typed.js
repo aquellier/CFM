5f5c410f807644b1044f059133e894bd8b0c2f68
antoinequellier <antoinecquellier@gmail.com>
2018-12-02 22:47:14 +0000

    fixed map on index

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -1,1 +1,15 @@
-typed.js
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  const typedBanner = document.getElementById('js-typed-text');
+  if (typedBanner) {
+    console.log('hello from typed');
+    new Typed(typedBanner, {
+      strings: ["anytime", "anywhere"],
+      typeSpeed: 100,
+      loop: true
+    });
+  }
+}
+
+export { loadDynamicBannerText };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,14 @@ import "bootstrap";
 import { navbarScroll } from '../components/navbar';
 import  { initMapShow, initMapIndex } from '../components/map.js';
 import { calendar } from '../components/flatpickr';
+import { loadDynamicBannerText } from '../components/typed';
 
 // Pages
 const indexPage = document.querySelector('.games.index');
 const showPage = document.querySelector('.games.show');
 
+// Functions
+loadDynamicBannerText();
 navbarScroll();
 if (indexPage) {
   initMapIndex();
