3443b1a0df2766b2026a2b920ceb1ab1bb29d32a
antoinequellier <antoinecquellier@gmail.com>
2018-12-05 08:39:51 +0000

    Omniauth migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # Array of providers we can sign in with
+  # Better to choose just one of them to avoid confusion
+  devise :omniauthable, omniauth_providers: [:facebook]
   has_many :bookings, dependent: :destroy
   mount_uploader :photo, PhotoUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_170444) do
+ActiveRecord::Schema.define(version: 2018_12_05_083834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,11 @@ ActiveRecord::Schema.define(version: 2018_12_03_170444) do
     t.integer "weight"
     t.string "phone_number"
     t.string "photo_fake"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
