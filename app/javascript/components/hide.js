cabd913cb75d9eebdbf3df27cacd190ec7951a6b
antoinequellier <antoinecquellier@gmail.com>
2019-06-04 16:20:53 +0100

    correct javascript map hide/show

--- app/javascript/components/hide.js
+++ app/javascript/components/hide.js
@@ -1,19 +1,19 @@
-function hideShow() {
-  const mapIndex = document.getElementById('map-index');
-  const mapIndexContainer = document.getElementById('map-index-container');
-  const button = document.getElementById('show-map');
-  const gamesContainer = document.querySelector('.games-container');
+const mapIndex = document.getElementById('map-index');
+const mapIndexContainer = document.getElementById('map-index-container');
+const button = document.getElementById('show-map');
+const gamesContainer = document.querySelector('.games-container');
 
-  const hideElement = (element) => {
-    if (element.style.display === "none") {
-      element.style.display = "block";
-      window.isMapOpen = true;
-    } else {
-      element.style.display = "none";
-      window.isMapOpen = false;
-    }
+const hideElement = (element) => {
+  if (element.style.display === "none") {
+    element.style.display = "block";
+    window.isMapOpen = true;
+  } else {
+    element.style.display = "none";
+    window.isMapOpen = false;
   }
+}
 
+function hideShow() {
   if (mapIndex) {
     console.log('hello from hide');
     window.isMapOpen = true;
