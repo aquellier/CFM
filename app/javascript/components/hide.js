181e913adeeb2eb329f8b420790b9f8e0767f020
antoinequellier <antoinecquellier@gmail.com>
2019-06-04 16:18:48 +0100

    change opacity background

--- app/javascript/components/hide.js
+++ app/javascript/components/hide.js
@@ -3,19 +3,20 @@ function hideShow() {
   const mapIndexContainer = document.getElementById('map-index-container');
   const button = document.getElementById('show-map');
   const gamesContainer = document.querySelector('.games-container');
+
+  const hideElement = (element) => {
+    if (element.style.display === "none") {
+      element.style.display = "block";
+      window.isMapOpen = true;
+    } else {
+      element.style.display = "none";
+      window.isMapOpen = false;
+    }
+  }
+
   if (mapIndex) {
     console.log('hello from hide');
     window.isMapOpen = true;
-    const hideElement = (element) => {
-      if (element.style.display === "none") {
-        element.style.display = "block";
-        window.isMapOpen = true;
-      } else {
-        element.style.display = "none";
-        window.isMapOpen = false;
-      }
-    }
-
     button.addEventListener('click', (event) => {
     const cards = document.querySelectorAll('.card');
       hideElement(mapIndex);
