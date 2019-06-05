a6b64af0f2e1bd3fcd48d4a5a62d516610fc5db5
antoinequellier <antoinecquellier@gmail.com>
2019-06-05 11:31:05 +0100

    redesign switch button

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,11 +1,15 @@
-function autocomplete() {
-  console.log('hello from autocomplete');
-  const gameLocation = document.getElementById('game_location');
-  if (gameLocation) {
-    const place = new google.maps.places.SearchBox(gameLocation, { types: [ 'geocode' ] });
-    google.maps.event.addDomListener(place, 'keydown');
+const initAutocomplete = () => {
+  const addressInput = document.getElementById('game_location') ||
+                       document.getElementById('address_query');
+  if (addressInput) {
+    completeSearch(addressInput);
   };
 };
 
+const completeSearch = (input) => {
+  const place = new google.maps.places.SearchBox(input, { types: [ 'geocode' ] });
+  google.maps.event.addDomListener(place, 'keydown');
+}
 
-export { autocomplete };
+
+export { initAutocomplete };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,13 @@
 import "bootstrap";
 import './application.css';
 import { navbarScroll } from '../components/navbar';
-import  { initMapShow, initMapIndex } from '../components/map.js';
+import { initMapShow, initMapIndex } from '../components/map.js';
 import { calendar } from '../components/flatpickr';
 import { hideShow } from '../components/hide.js';
 import { loadDynamicBannerText } from '../components/typed';
 import { fadeOut, changeButton } from '../components/banner';
 import { hideOnSmallScreen } from '../components/smallscreen';
-import { autocomplete } from '../components/autocomplete';
+import { initAutocomplete } from '../components/autocomplete';
 // Pages
 const indexPage = document.querySelector('.games.index');
 const showPage = document.querySelector('.games.show');
@@ -27,4 +27,4 @@ if (showPage) {
   initMapShow();
 }
 calendar();
-autocomplete();
+initAutocomplete();
