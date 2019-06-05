530c010c4aac1cd117a9c77c5b16ed17cee1c03a
antoinequellier <antoinecquellier@gmail.com>
2019-06-05 18:56:02 +0100

    select games based on address

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,12 @@ class GamesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   before_action :set_game, only: [:show, :edit, :update, :destroy]
   def index
-    @games = Game.where(:datetime => Date.today..Date.today + 14).order(:datetime)
+    @games = Game.where(datetime: Date.today..Date.today + 14)
+    if params[:address_query].present?
+      fields = Field.near(params[:address_query], 50, order: 'distance')
+      @games = @games.select { |game| fields.include?(game.field)}
+    end
+
     @markers = @games.map do |game|
       {
         lng: game.field.longitude,
@@ -60,16 +65,6 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
-  def define_location
-    if @game.field.location == "UrbanSoccer - Porte d'Aubervilliers, Avenue Victor Hugo, Aubervilliers, France"
-      @game.field = "Porte d'Aubervilliers"
-    elsif @game.field.location == "Praça de Espanha"
-      @game.field = "Praça de Espanha, Lisboa"
-    elsif @game.field == "Anjos"
-      @game.field.location = "Anjos, Lisboa"
-    elsif @game.field.location == "Campo Pequeno"
-      @game.field = "Campo Pequeno,Lisboa"
-    end
-  end
+
 
 end
