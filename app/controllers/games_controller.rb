6da26e4e10af759721bbd0889ff9985bf23b32b1
antoinequellier <antoinecquellier@gmail.com>
2019-08-12 17:28:04 +0200

    ajaxify index page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,17 @@
 class GamesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   before_action :set_game, only: [:show, :edit, :update, :destroy]
+
   def index
     @games = Game.where(datetime: Date.today..Date.today + 14)
     fields = @games.map(&:field)
     if params[:address_query].present?
-      fields = Field.near(params[:address_query], 50, order: 'distance')
-      @games = @games.select { |game| fields.include? game.field }
+      respond_to do |format|
+        fields = Field.near(params[:address_query], 50, order: 'distance')
+        @games = @games.select { |game| fields.include? game.field }
+        format.html { redirect_to games_path }
+        format.js
+      end
     end
     get_markers(@games)
   end
--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -1,51 +1,50 @@
 import GMaps from 'gmaps/gmaps.js';
 import styles from './styles.js';
 
+function addMarkers(map, markers) {
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+}
+
+function addStyle(map) {
+  map.addStyle({
+    styles: styles,
+    mapTypeId: 'map_style'
+  });
+  map.setStyle('map_style');
+}
+
+
 function initMapShow() {
-  console.log('hello from map show');
   const mapShow = document.getElementById('map-show');
   if (mapShow) { // don't try to build a map if there's no div#map to inject in
     const map = new GMaps({ el: '#map-show', lat: 0, lng: 0 });
     const markers = JSON.parse(mapShow.dataset.markers);
-    map.addMarkers(markers);
-    if (markers.length === 0) {
-      map.setZoom(2);
-    } else if (markers.length === 1) {
-      map.setCenter(markers[0].lat, markers[0].lng);
-      map.setZoom(14);
-    } else {
-      map.fitLatLngBounds(markers);
-    }
-
-    map.addStyle({
-      styles: styles,
-      mapTypeId: 'map_style'
-    });
-    map.setStyle('map_style');
+    addMarkers(map, markers);
+    addStyle(map);
   }
 }
 
 function initMapIndex() {
-  console.log('hello from map index');
   const mapIndex = document.getElementById('map-index');
   if (mapIndex) { // don't try to build a map if there's no div#map to inject in
     const map = new GMaps({ el: '#map-index', lat: 0, lng: 0 });
     const markers = JSON.parse(mapIndex.dataset.markers);
-    map.addMarkers(markers);
-    if (markers.length === 0) {
-      map.setZoom(2);
-    } else if (markers.length === 1) {
-      map.setCenter(markers[0].lat, markers[0].lng);
-      map.setZoom(14);
-    } else {
-      map.fitLatLngBounds(markers);
+    addMarkers(map, markers);
+    addStyle(map);
+    function updateMarkers(markersData) {
+      map.removeMarkers(markers);
+      addMarkers(map, markersData);
+      console.log(markersData);
     }
-
-    map.addStyle({
-      styles: styles,
-      mapTypeId: 'map_style'
-    });
-    map.setStyle('map_style');
+    window.updateMarkers = updateMarkers;
   }
 }
 
